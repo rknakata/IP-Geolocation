fbe5cf41b50376b728ca0e17d577a64b99ee7242
Ryan Nakata <rknakata@hawaii.edu>
2016-12-20 07:12:36 -1000

    first commit

--- iplocate.rb
+++ iplocate.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'open-uri'
 
 ip = ARGV.first
-
-city = (open("http://ipinfo.io/#{ip}/city").read)
-
-puts city
+if ip != nil
+  city = (open("http://ipinfo.io/#{ip}/city").read)
+  puts city
+end
